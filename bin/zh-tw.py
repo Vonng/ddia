7bce759c4415b45c25e2a17ab45cad180a2a3f3c
Gang Yin <1246410+yingang@users.noreply.github.com>
2021-12-07 14:29:51 +0800

    update zh-tw.py to handle the translation issues with 髮 and 發

--- bin/zh-tw.py
+++ bin/zh-tw.py
@@ -4,7 +4,13 @@ import os, sys, opencc
 def convert(src_path, dst_path, cfg='s2twp.json'):
     converter = opencc.OpenCC(cfg)
     with open(src_path, "r", encoding='utf-8') as src, open(dst_path, "w+", encoding='utf-8') as dst:
-        dst.write("\n".join(converter.convert(line.rstrip()).replace('(img/', '(../img/') for line in src))
+        dst.write("\n".join(
+            converter.convert(line.rstrip()).replace('(img/', '(../img/')
+                .replace('髮送', '傳送')
+                .replace('髮布', '釋出')
+                .replace('髮生', '發生')
+                .replace('髮出', '發出')
+                for line in src))
     print("convert %s to %s" % (src_path, dst_path))
 
 
