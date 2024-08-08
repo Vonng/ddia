5908c6207b37ab3f7521a0135d007235d6e17959
Gang Yin <1246410+yingang@users.noreply.github.com>
2024-08-08 17:12:43 +0800

    fix zh-tw.py and add comments

--- bin/zh-tw.py
+++ bin/zh-tw.py
@@ -11,14 +11,14 @@ def convert(src_path, dst_path, cfg='s2twp.json'):
                 .replace('髮生', '發生')
                 .replace('髮出', '發出')
                 .replace('嚐試', '嘗試')
-                .replace('線上性', '線性')
+                .replace('線上性一致', '在線性一致')    # 优先按“在线”解析了？
                 .replace('復雜', '複雜')
                 .replace('討論瞭', '討論了')
                 .replace('倒黴', '倒楣')
                 .replace('區域性性', '區域性')
-                .replace('下麵', '下面')
-                .replace('日志', '日誌')
-                .replace('真即時間', '真實時間')
+                .replace('下麵條件', '下面條件')        # 优先按“面条”解析了？
+                .replace('當日志', '當日誌')            # 优先按“当日”解析了，没有考虑后面的“日志”？
+                .replace('真即時間', '真實時間')        # 优先按“实时”解析了，没有考虑前面的“真实”？
                 for line in src))
     print("convert %s to %s" % (src_path, dst_path))
 
